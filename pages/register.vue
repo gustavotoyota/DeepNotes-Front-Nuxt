ef8b8b435816adb829eec1c546906bc816ce3b82
Gustavo Toyota <gustavotakachitoyota@gmail.com>
2022-01-27 14:14:10 -0400

    Add first name and last name to sign up

--- pages/register.vue
+++ pages/register.vue
@@ -1,6 +1,24 @@
 <template>
   <div style="padding: 20px">
     <form @submit.prevent="onSubmit">
+      <div style="display: flex">
+        <v-text-field
+        label="First name"
+        id="first-name"
+        dense
+        outlined
+        v-model="loginData.firstName"/>
+
+        <Gap width="20px"/>
+
+        <v-text-field
+        label="Last name"
+        id="last-name"
+        dense
+        outlined
+        v-model="loginData.lastName"/>
+      </div>
+
       <v-text-field
       label="E-mail"
       id="email"
@@ -14,6 +32,14 @@
       dense
       outlined
       v-model="loginData.password"
+      type="password"/>
+    
+      <v-text-field
+      label="Repeat password"
+      id="repeat-password"
+      dense
+      outlined
+      v-model="loginData.repeatPassword"
       type="password"/>
 
       <v-btn
@@ -42,6 +68,11 @@ export default {
   methods: {
 
     async onSubmit() {
+      if (this.loginData.password !== this.loginData.repeatPassword) {
+        alert('Passwords don\'t match')
+        return
+      }
+
       try {
         const response = await this.$axios.post('/auth/register', this.loginData)
       
