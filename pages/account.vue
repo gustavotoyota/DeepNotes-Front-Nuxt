9042daf75d03c2d1827b6fea0bed780025ae4c69
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-01-29 14:40:34 -0400

    Improve account page

--- pages/account.vue
+++ pages/account.vue
@@ -1,13 +1,56 @@
 <template>
-  <v-main>
-    <div v-if="$auth.user.newEmail">
-      Unconfirmed e-mail: {{ $auth.user.newEmail }}
-      <v-btn @click="resendEmailLink">Resend link</v-btn>
-      <v-btn @click="cancelEmailChange">Cancel change</v-btn>
-    </div>
-    <div>E-mail: {{ $auth.user.email }} <v-btn @click="changeEmail">Change</v-btn></div>
-    <div>Password: ******** <v-btn @click="changePassword">Change</v-btn></div>
-  </v-main>
+  <div style="display: contents">
+    <v-navigation-drawer
+    clipped
+    app
+    permanent>
+
+      <v-list>
+
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-account-circle</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            Profile
+          </v-list-item-title>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-shield</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            Security
+          </v-list-item-title>
+        </v-list-item>
+
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            Settings
+          </v-list-item-title>
+        </v-list-item>
+        
+      </v-list>
+
+    </v-navigation-drawer>
+
+
+    <v-main>
+      <div style="padding: 32px">
+        <div v-if="$auth.user.newEmail">
+          Unconfirmed e-mail: {{ $auth.user.newEmail }}
+          <v-btn @click="resendEmailLink">Resend link</v-btn>
+          <v-btn @click="cancelEmailChange">Cancel change</v-btn>
+        </div>
+        <div>E-mail: {{ $auth.user.email }} <v-btn @click="changeEmail">Change e-mail</v-btn></div>
+        <div>Password: ******** <v-btn @click="changePassword">Change password</v-btn></div>
+      </div>
+    </v-main>
+  </div>
 </template>
 
 <script setup>
