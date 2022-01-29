d22ad6b09f0d47842cf8db171bd413d0f7e5edd4
Gustavo Takachi Toyota <gustavotakachitoyota@gmail.com>
2022-01-29 13:38:35 -0400

    Fix footer attributes

--- layouts/default.vue
+++ layouts/default.vue
@@ -82,13 +82,17 @@
 
 
     <v-footer
+    height="180"
     app
     dark
+    absolute
     padless>
       <v-card
       flat
       tile
-      class="indigo lighten-1 white--text text-center">
+      class="fill-height 
+      indigo lighten-1
+      white--text text-center">
         <v-card-text class="white--text pt-0">
           Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
         </v-card-text>
